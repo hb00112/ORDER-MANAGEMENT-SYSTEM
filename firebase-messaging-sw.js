512c5d0dec2f9deb97ef0f92a8fa0eb117b8c283
hb00112 <159218387+hb00112@users.noreply.github.com>
2024-08-22 09:27:13 +0530

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,25 @@
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/9.0.0/firebase-messaging-compat.js');
+
+firebase.initializeApp({
+  apiKey: "AIzaSyB8fDn9EduVJsx8unmUHo74TsT3fgWGMAM",
+  authDomain: "lovable-order-database.firebaseapp.com",
+  databaseURL: "https://lovable-order-database-default-rtdb.firebaseio.com",
+  projectId: "lovable-order-database",
+  storageBucket: "lovable-order-database.appspot.com",
+  messagingSenderId: "814278451797",
+  appId: "1:814278451797:web:236a8fc04a7f1762156adc",
+  measurementId: "G-15VMWPH500"
+});
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage((payload) => {
+  console.log('Received background message ', payload);
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+  };
+
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
