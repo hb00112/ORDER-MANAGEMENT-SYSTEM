9f32555abaee733f8575156f6d9b557483394bf2
hb00112 <159218387+hb00112@users.noreply.github.com>
2024-10-02 17:45:22 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,6 @@ const CACHE_NAME = 'ka-oms-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
-  '/styles.css',
   '/manifest.json',
   'https://res.cloudinary.com/dfb8lszpo/video/upload/v1727802463/xtlzfrrwrmnnymz5tz43.mp4'
 ];
