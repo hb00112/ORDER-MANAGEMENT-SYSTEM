d5aacdc68c0601de5c47a99df2f7c7915ab42d56
hb00112 <159218387+hb00112@users.noreply.github.com>
2024-10-02 16:26:46 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -1,31 +1,29 @@
-
-const CACHE_NAME = 'pwa-test-cache-v1';
+const CACHE_NAME = 'ka-oms-cache-v1';
 const urlsToCache = [
   '/',
   '/index.html',
+  '/styles.css',
   '/manifest.json',
-  'https://s3.ezgif.com/tmp/ezgif-3-092ebae015.png',
   'https://res.cloudinary.com/dfb8lszpo/video/upload/v1727802463/xtlzfrrwrmnnymz5tz43.mp4'
 ];
+
 self.addEventListener('install', (event) => {
   event.waitUntil(
-    caches.open('your-cache-name').then((cache) => {
-      return cache.addAll([
-        '/',
-        '/index.html',
-        'https://res.cloudinary.com/dfb8lszpo/video/upload/v1727802463/xtlzfrrwrmnnymz5tz43.mp4',
-        // other assets...
-      ]);
-    })
-  );
-});
-
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
+    caches.open(CACHE_NAME)
+      .then((cache) => {
+        return cache.addAll(urlsToCache);
+      })
   );
 });
 
-
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => {
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      })
+  );
+});
